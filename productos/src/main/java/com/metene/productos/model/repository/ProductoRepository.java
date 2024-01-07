afa401f6bbcb5833c6b5e6a702b961318ab5826f
juan.metenenguema <juan.metenenguema@externos.correo.gob.es>
2024-01-07 18:05:43 +0100

    Refactorizando el nombre del método

--- productos/src/main/java/com/metene/productos/model/repository/ProductoRepository.java
+++ productos/src/main/java/com/metene/productos/model/repository/ProductoRepository.java
@@ -12,12 +12,20 @@ import com.metene.productos.model.entity.ProductoPK;
 
 public interface ProductoRepository extends CrudRepository<Producto, ProductoPK> {
 
+	/**
+	 * Método que busca un producto por parámetros personalizados
+	 * @param brandId Identificador de la marca
+	 * @param productId Identificador del producto
+	 * @param startDate Fecha de inicio de vigencia del producto
+	 * @param endDate Fecha de fin de vigencia del producto
+	 * @return Producto que cumple con los parámetros de búsqueda
+	 */
 	@Query("SELECT p FROM Producto "
 			+ "WHERE p.brandId =: brandId AND "
 			+ "p.productoPK.productId =: productId "
 			+ "AND p.productoPK.startDate =: startDate AND "
 			+ "p.productoPK.endDate =: endDate")
-	Optional<Producto> findByDateAndProductIdAndBrandId(
+	Optional<Producto> findByCustomParameters(
 			@Param("brandId") String brandId,
 			@Param("productId") Long productId, 
 			@Param("startDate") Date startDate, 

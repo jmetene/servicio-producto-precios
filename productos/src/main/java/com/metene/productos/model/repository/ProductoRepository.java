0df82810170c5df93bd2449dd65b1d89f4375bf6
juan.metenenguema <juan.metenenguema@externos.correo.gob.es>
2024-01-07 17:58:52 +0100

    Creación de la query de consulta del producto

--- productos/src/main/java/com/metene/productos/model/repository/ProductoRepository.java
+++ productos/src/main/java/com/metene/productos/model/repository/ProductoRepository.java
@@ -1,11 +1,25 @@
 package com.metene.productos.model.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.metene.productos.model.entity.Producto;
 import com.metene.productos.model.entity.ProductoPK;
 
 public interface ProductoRepository extends CrudRepository<Producto, ProductoPK> {
 
+	@Query("SELECT p FROM Producto "
+			+ "WHERE p.brandId =: brandId AND "
+			+ "p.productoPK.productId =: productId "
+			+ "AND p.productoPK.startDate =: startDate AND "
+			+ "p.productoPK.endDate =: endDate")
+	Optional<Producto> findByDateAndProductIdAndBrandId(
+			@Param("brandId") String brandId,
+			@Param("productId") Long productId, 
+			@Param("startDate") Date startDate, 
+			@Param("endDate") Date endDate);
 }
